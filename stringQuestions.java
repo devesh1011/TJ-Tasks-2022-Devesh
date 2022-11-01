6c69ea607ea0455ce68c14454badf0a8a1753cbe
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2022-11-01 17:23:42 +0530

    Added a new file

---
+++ stringQuestions.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class stringQuestions {
+    public static void checkPalindrome(String str) {
+        str.toCharArray();
+
+        for (int i = str.length(); i > 0; i++) {
+            
+        }
+
+        System.out.println(str);
+    }
+
+    public static void main(String args[]) {
+        String str = "Devesh";
+
+        checkPalindrome(str);
+    }
+}
